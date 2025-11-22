9fd060c59c1ba5a734a0f9385d2bf4b228a3cea8
root <root@marie-lou.allain.13h37.io>
2025-11-22 15:48:07 +0100

    Stop tracking config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'music_community');
-define('DB_USER', 'myuser');
-define('DB_PASS', 'mypassword');
-
-// App settings
-define('BASE_URL', '/'); // Adjust if app is in subfolder
