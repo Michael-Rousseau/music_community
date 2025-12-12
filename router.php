fc8494841d86ed05cac1aadd1af5000d990adc9d
michael-rousseau <michael.rousseau@flowdesk.co>
2025-12-12 16:22:43 +0100

    ffix

--- router.php
+++ router.php
@@ -1,14 +1,11 @@
 <?php
-// router.php (At the root of the project)
 $publicFolder = __DIR__ . '/public';
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $file = $publicFolder . $path;
 
-// If the file exists (like an image or CSS), serve it directly
 if (is_file($file)) {
-    return false; 
+    return false; // serve the file normally
 }
 
-// Otherwise, send everything to index.php
 require $publicFolder . '/index.php';
