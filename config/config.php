782f0a4eb96c1505ab6aed0c3ad496f0892d8cb3
michael-rousseau <michael.rousseau@flowdesk.co>
2025-12-12 14:54:15 +0100

    add env var

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'users'); 
+define('DB_USER', 'root');
+define('DB_PASS', 'Collector10'); 
+
+define('BASE_URL', 'http://localhost/music_community/public');
