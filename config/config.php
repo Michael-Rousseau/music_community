d3d5bd2503b8f16bb562fac2a7234cc4d538c2d5
michael-rousseau <michael.rousseau@flowdesk.co>
2025-12-13 15:33:57 +0100

    fix

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'users'); 
 define('DB_USER', 'root');
-define('DB_PASS', 'Collector10'); 
+define('DB_PASS', 'Collector10'); // rtyrty
 
 define('BASE_URL', '/');
