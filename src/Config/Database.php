419d086aa88fc09586594568fd382b10b72e40bf
michael-rousseau <michael.rousseau@flowdesk.co>
2026-01-04 12:38:56 +0100

    add env

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -1,18 +1,21 @@
-<?php
 namespace App\Config;
 
 use PDO;
 use PDOException;
+use Dotenv\Dotenv; 
 
 class Database {
     private static $pdo = null;
 
     public static function getConnection() {
         if (self::$pdo === null) {
-            $host = 'localhost';
-            $db   = 'music_community';
-            $user = 'root';
-            $pass = 'Collector10'; 
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+            $dotenv->safeLoad();
+
+            $host = $_ENV['DB_HOST'] ?? 'localhost';
+            $db   = $_ENV['DB_NAME'] ?? 'music_community';
+            $user = $_ENV['DB_USER'] ?? 'root';
+            $pass = $_ENV['DB_PASS'] ?? '';
 
             try {
                 self::$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
